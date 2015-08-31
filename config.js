2e5b5b6d2483cfa3f84f44dd9c838a087ea6b5c6
Ali Ijaz Sheikh <ofrobots@google.com>
2015-08-31 09:08:48 -0700

    fix waitToken + misc. fixes and cleanups
    Switch over completely to the v2 API, and get rid of expiry logic.
    Correct crash in remove breakpoint when using an unsupported node.js version.
    Other minor cleanups.

--- config.js
+++ config.js
@@ -19,38 +19,51 @@
 module.exports = {
   enabled: process.env.GCLOUD_DEBUG_DISABLED || true,
   workingDirectory: process.cwd(),
-  // Log levels: 0-disabled,1-error,2-warn,3-info,4-debug
+
+  // Log levels: 0-disabled,1-error,2-warn,3-info,4-debug.
   logLevel: process.env.GCLOUD_DEBUG_LOGLEVEL || 1,
+
   // How frequently should the list of breakpoints be refreshed from the
-  // cloud debug server
+  // cloud debug server.
   breakpointUpdateIntervalSec: 10,
+
   // We expire stale breakpoints on the server.
   breakpointExpirationSec: 60 * 60 * 24, // 24 hours
 
   capture: {
-    // Whether to include details about stack frames belonging to node-core
+    // Whether to include details about stack frames belonging to node-core.
     includeNodeModules: false,
+
     // Maximum number of stack frames to capture data for. The limit is aimed
     // to reduce overall capture time
     // TODO: implement this
     //maxFrames: 20,
+
     // Only collect locals and arguments on a few top frames. For the rest
     // only collect the source location
     // TODO: implement this
     //maxExpandFrames: 5,
+
     // To reduce the overall capture time, limit the number of properties
     // gathered on large object. A value of 0 disables the limit.
     maxProperties: 0,
+
     // Total 'size' if data to gather. This is NOT the number of bytes of data
     // that are sent over the wire, but instead a very very coarse approximation
     // based on the length of names and values of the properties. This should
     // be somewhat proportional to the amount of processing needed to capture
-    // the data and subsequently the network traffic
+    // the data and subsequently the network traffic.
     maxDataSize: 20000
+
     // To limit the size of the buffer, we truncate long strings.
     // A value of 0 disables truncation.
     // TODO: implement
     //maxStringLength: 80
-  }
+  },
+
+// These configuration options are for internal experimentation only.
+internal: {
+  registerDelayOnFetcherErrorSec: 300 // 5 minutes.
+}
 
 };
