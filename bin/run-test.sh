84713229990301da9bbfaea78348df8aa7eaf217
Ali Ijaz Sheikh <ofrobots@google.com>
2015-10-29 14:54:12 -0700

    Add test for GCLOUD_DEBUG_DISABLED
    Adding a test that should fail to plug a hole in our coverage.

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -2,3 +2,8 @@
 
 jshint . || exit 1
 istanbul test $(npm bin)/_mocha -- test --timeout 4000 --R spec || exit 1
+for test in test/standalone/test-*.js ;
+do
+  echo "Running standalone test: ${test}"
+  istanbul test $(npm bin)/_mocha -- ${test} --timeout 4000 --R spec || exit 1
+done
