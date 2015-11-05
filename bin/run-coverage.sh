a1ae26f5e29c3d3e5977b69260d3da6ea92670ab
Matt Loring <mattloring@google.com>
2015-11-05 09:57:35 -0800

    Improve coverage reporting

--- bin/run-coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-istanbul cover $(npm bin)/_mocha --report lcovonly -- test --timeout 4000 --R spec
-cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
-rm -rf ./coverage
--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,9 +1,44 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Usage: -c to report coverage
+
+while true; do
+  case $1 in
+    -c)
+      cover=1
+      ;;
+
+    *)
+      break
+  esac
+
+  shift
+done
+
+# Lint
 jshint . || exit 1
-istanbul test $(npm bin)/_mocha -- test --timeout 4000 --R spec || exit 1
+
+# Get test/coverage command
+counter=0
+function run {
+  C="$(npm bin)/istanbul test"
+  if [ "$cover" ]; then
+    C="$(npm bin)/istanbul cover --dir ./coverage/${counter}"
+    ((counter++))
+  fi
+  ($C "$(npm bin)/_mocha" -- $* --timeout 4000 --R spec) || exit 1
+}
+
+# Run test/coverage
+run test
 for test in test/standalone/test-*.js ;
 do
-  echo "Running standalone test: ${test}"
-  istanbul test $(npm bin)/_mocha -- ${test} --timeout 4000 --R spec || exit 1
+  run "${test}"
 done
+
+# Conditionally publish coverage
+if [ "$cover" ]; then
+  istanbul report lcovonly
+  ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
+  rm -rf ./coverage
+fi
