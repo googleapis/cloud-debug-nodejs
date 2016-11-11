50d837bff5a680929a5ee504668d81d5d700d5db
Dominic Kramer <DominicDKramer@google.com>
2016-11-11 11:37:36 -0800

    Evaluated expressions respect capture.maxProperties
    Now whether or not a captured variable is an evaluated
    expression, the `config.capture.maxProperties` configuration will
    be respected when displaying the contents of the variable.

--- test/test-v8debugapi.js
+++ test/test-v8debugapi.js
@@ -743,42 +743,58 @@ describe('v8debugapi', function() {
       });
     });
 
-    it('should not limit the length of an evaluated array based on maxProperties',
-      function(done) {
-        var bp = {
-          id: 'fake-id-124',
-          location: { path: 'test-v8debugapi.js', line: 5 },
-          expressions: ['A']
-        };
-        var oldMaxProps = config.capture.maxProperties;
-        var oldMaxData = config.capture.maxDataSize;
-        config.capture.maxProperties = 1;
-        config.capture.maxDataSize = 20000;
-        api.set(bp, function(err) {
+    it('should limit array length', function(done) {
+      var bp = {
+        id: 'fake-id-124',
+        location: { path: 'test-v8debugapi.js', line: 5 },
+        expressions: ['A']
+      };
+      var oldMax = config.capture.maxProperties;
+      config.capture.maxProperties = 1;
+      api.set(bp, function(err) {
+        assert.ifError(err);
+        api.wait(bp, function(err) {
           assert.ifError(err);
-          api.wait(bp, function(err) {
-            assert.ifError(err);
-            var foo = bp.evaluatedExpressions[0];
-            var fooVal = bp.variableTable[foo.varTableIndex];
-            if (semver.satisfies(process.version, '<1.6')) {
-              // In v0.12 there are members for the attributes
-              // '1', '2', and '3'
-              assert.equal(fooVal.members.length, 3);
-            }
-            else {
-              // After v0.12 there are members for the attributes 
-              // '1', '2', '3', and 'length'
-              assert.equal(fooVal.members.length, 4);
-            }
-            assert.strictEqual(foo.status, undefined);
+          var foo = bp.evaluatedExpressions[0];
+          var fooVal = bp.variableTable[foo.varTableIndex];
+          assert.equal(fooVal.members.length, 1);
+          assert(foo.status.description.format.indexOf(
+            'Only first') !== -1);
+          assert(!foo.status.isError);
 
-            api.clear(bp);
-            config.capture.maxDataSize = oldMaxData;
-            config.capture.maxProperties = oldMaxProps;
-            done();
-          });
-          process.nextTick(function() {foo(2);});
+          api.clear(bp);
+          config.capture.maxProperties = oldMax;
+          done();
         });
+        process.nextTick(function() {foo(2);});
+      });
+    });
+
+    it('should limit object length', function(done) {
+      var bp = {
+        id: 'fake-id-124',
+        location: { path: 'test-v8debugapi.js', line: 5 },
+        expressions: ['B']
+      };
+      var oldMax = config.capture.maxProperties;
+      config.capture.maxProperties = 1;
+      api.set(bp, function(err) {
+        assert.ifError(err);
+        api.wait(bp, function(err) {
+          assert.ifError(err);
+          var foo = bp.evaluatedExpressions[0];
+          var fooVal = bp.variableTable[foo.varTableIndex];
+          assert.equal(fooVal.members.length, 1);
+          assert(foo.status.description.format.indexOf(
+            'Only first') !== -1);
+          assert(!foo.status.isError);
+
+          api.clear(bp);
+          config.capture.maxProperties = oldMax;
+          done();
+        });
+        process.nextTick(function() {foo(2);});
+      });
     });
 
     it('should display an error for an evaluated array beyond maxDataSize',
@@ -801,35 +817,6 @@ describe('v8debugapi', function() {
             assert(fooVal.status.description.format.indexOf(
               'Max data size reached') !== -1);
             assert(fooVal.status.isError);
-
-            api.clear(bp);
-            config.capture.maxDataSize = oldMaxData;
-            config.capture.maxProperties = oldMaxProps;
-            done();
-          });
-          process.nextTick(function() {foo(2);});
-        });
-    });
-
-    it('should not limit the length of an evaluated object based on maxProperties',
-      function(done) {
-        var bp = {
-          id: 'fake-id-124',
-          location: { path: 'test-v8debugapi.js', line: 5 },
-          expressions: ['B']
-        };
-        var oldMaxProps = config.capture.maxProperties;
-        var oldMaxData = config.capture.maxDataSize;
-        config.capture.maxProperties = 1;
-        config.capture.maxDataSize = 20000;
-        api.set(bp, function(err) {
-          assert.ifError(err);
-          api.wait(bp, function(err) {
-            assert.ifError(err);
-            var foo = bp.evaluatedExpressions[0];
-            var fooVal = bp.variableTable[foo.varTableIndex];
-            assert.equal(fooVal.members.length, 3);
-            assert.strictEqual(foo.status, undefined);
 
             api.clear(bp);
             config.capture.maxDataSize = oldMaxData;
