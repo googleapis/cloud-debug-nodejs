2ec17329fb74b7cb46bbbadf1c9d2a8cd6294d0c
Kelvin Jin <kelvinjin@google.com>
2016-12-22 15:20:05 -0800

    Change argument order in updateBreakpoint (#204)

--- test/test-controller.js
+++ test/test-controller.js
@@ -243,7 +243,7 @@ describe('Controller API', function() {
         .reply(200, { kind: 'debugletcontroller#updateActiveBreakpointResponse'});
       var debuggee = { id: 'fake-debuggee' };
       var controller = new Controller(fakeDebug);
-      controller.updateBreakpoint(breakpoint, debuggee,
+      controller.updateBreakpoint(debuggee, breakpoint,
         function(err, result) {
           assert(!err, 'not expecting an error');
           assert.equal(result.kind, 'debugletcontroller#updateActiveBreakpointResponse');
