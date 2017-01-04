2ac939f132f791986b44484715d8308302a57cd5
Kelvin Jin <kelvinjin@google.com>
2017-01-04 14:56:06 -0800

    Corrected run-test.sh

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -18,33 +18,32 @@ done
 # Lint
 $(npm bin)/jshint . || exit 1
 
+# Get test/coverage command
+counter=0
+function run {
+  C="$(npm bin)/istanbul test"
+  if [ "$cover" ]; then
+    C="$(npm bin)/istanbul cover --dir ./coverage/${counter}"
+    ((counter++))
+  fi
+  ($C "$(npm bin)/_mocha" -- $* --timeout 4000 --R spec) || exit 1
+}
 
+# Run test/coverage
+run test
+for test in test/standalone/test-*.js ;
+do
+  run "${test}"
+done
+
+# Conditionally publish coverage
+if [ "$cover" ]; then
+  $(npm bin)/istanbul report lcovonly
+  ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
+  rm -rf ./coverage
+fi
 
 if [ -z "${TRAVIS_PULL_REQUEST}" ] || [ "${TRAVIS_PULL_REQUEST}" = "false" ]
-  # Get test/coverage command
-  counter=0
-  function run {
-    C="$(npm bin)/istanbul test"
-    if [ "$cover" ]; then
-      C="$(npm bin)/istanbul cover --dir ./coverage/${counter}"
-      ((counter++))
-    fi
-    ($C "$(npm bin)/_mocha" -- $* --timeout 4000 --R spec) || exit 1
-  }
-
-  # Run test/coverage
-  run test
-  for test in test/standalone/test-*.js ;
-  do
-    run "${test}"
-  done
-
-  # Conditionally publish coverage
-  if [ "$cover" ]; then
-    $(npm bin)/istanbul report lcovonly
-    ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
-    rm -rf ./coverage
-  fi
 then
   if [ -z "${GCLOUD_PROJECT}" ]; then
     echo "============================================================"
