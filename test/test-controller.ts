c4022a68ab944d1e42ae0269912054544713913b
Dominic Kramer <DominicDKramer@google.com>
2017-07-15 15:01:47 -0700

    Fix tests associated with `test-controller.ts`

--- test/test-controller.ts
+++ test/test-controller.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
 
 var assert = require('assert');
 var nock   = require('nock');
