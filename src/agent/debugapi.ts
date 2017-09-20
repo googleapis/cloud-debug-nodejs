87c454e20c082452fb64e130048c2d01c16b3e99
Michael Gao <fgao@google.com>
2017-09-19 23:13:09 -0700

    nit

--- src/agent/debugapi.ts
+++ src/agent/debugapi.ts
@@ -24,26 +24,6 @@ import {ScanStats} from './scanner';
 import {SourceMapper} from './sourcemapper';
 import {V8DebugApi} from './v8debugapi';
 
-export const messages = {
-  INVALID_BREAKPOINT: 'invalid snapshot - id or location missing',
-  SOURCE_FILE_NOT_FOUND:
-      'A script matching the source file was not found loaded on the debuggee',
-  SOURCE_FILE_AMBIGUOUS: 'Multiple files match the path specified',
-  V8_BREAKPOINT_ERROR: 'Unable to set breakpoint in v8',
-  V8_BREAKPOINT_CLEAR_ERROR: 'Unable to clear breakpoint in v8',
-  SYNTAX_ERROR_IN_CONDITION: 'Syntax error in condition: ',
-  ERROR_EVALUATING_CONDITION: 'Error evaluating condition: ',
-  ERROR_COMPILING_CONDITION: 'Error compiling condition.',
-  DISALLOWED_EXPRESSION: 'Expression not allowed',
-  SOURCE_MAP_READ_ERROR:
-      'The source map could not be read or was incorrectly formatted',
-  V8_BREAKPOINT_DISABLED: 'Internal error: V8 breakpoint externally disabled',
-  CAPTURE_BREAKPOINT_DATA: 'Error trying to capture snapshot data: ',
-  INVALID_LINE_NUMBER: 'Invalid snapshot position: ',
-  COULD_NOT_FIND_OUTPUT_FILE:
-      'Could not determine the output file associated with the transpiled input file'
-};
-
 export interface DebugApi {
   set: (breakpoint: apiTypes.Breakpoint, cb: (err: Error|null) => void) => void;
   clear:
