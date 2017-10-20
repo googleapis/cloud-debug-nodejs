4b1f22fe2273bfc6392c23042cddeb5abc74cd52
Dominic Kramer <DominicDKramer@google.com>
2017-10-20 15:10:24 -0700

    Remove name_ parameter names

--- src/agent/v8/legacy-debugapi.ts
+++ src/agent/v8/legacy-debugapi.ts
@@ -60,14 +60,14 @@ export class V8DebugApi implements debugapi.DebugApi {
   numBreakpoints = 0;
 
   constructor(
-      logger_: Logger, config_: DebugAgentConfig, jsFiles_: ScanStats,
-      sourcemapper_: SourceMapper) {
-    this.sourcemapper = sourcemapper_;
+      logger: Logger, config: DebugAgentConfig, jsFiles: ScanStats,
+      sourcemapper: SourceMapper) {
+    this.sourcemapper = sourcemapper;
     this.v8 = vm.runInDebugContext('Debug');
-    this.config = config_;
-    this.fileStats = jsFiles_;
+    this.config = config;
+    this.fileStats = jsFiles;
     this.v8Version = /(\d+\.\d+\.\d+)\.\d+/.exec(process.versions.v8);
-    this.logger = logger_;
+    this.logger = logger;
     this.usePermanentListener = semver.satisfies(this.v8Version[1], '>=4.5');
     this.handleDebugEvents =
         (evt: v8.DebugEvent, execState: v8.ExecutionState,
