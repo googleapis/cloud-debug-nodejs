2c244ea514822accc8808ccea677793af12249ac
Dominic Kramer <DominicDKramer@google.com>
2017-11-14 12:56:31 -0800

    Address GitHub comments

--- test/test-install.ts
+++ test/test-install.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import * as assert from 'assert';
 import * as path from 'path';
@@ -14,7 +29,7 @@ const TS_CODE_3 = `import * as debug from '@google-cloud/debug-agent';
 debug.start({ allowExpressions: true });
 `;
 
-const TS_CODE = [TS_CODE_1, TS_CODE_2, TS_CODE_3];
+const TS_CODE_ARRAY = [TS_CODE_1, TS_CODE_2, TS_CODE_3];
 
 const JS_CODE_1 = `require('@google-cloud/debug-agent').start()`;
 const JS_CODE_2 = `require('@google-cloud/debug-agent').start({})`;
@@ -22,7 +37,7 @@ const JS_CODE_3 = `require('@google-cloud/debug-agent').start({
   allowExpressions: true
 })`;
 
-const JS_CODE = [JS_CODE_1, JS_CODE_2, JS_CODE_3];
+const JS_CODE_ARRAY = [JS_CODE_1, JS_CODE_2, JS_CODE_3];
 
 const TIMEOUT_MS = 60*1000;
 
@@ -67,8 +82,8 @@ describe('Installation', () => {
     }
   });
 
-  TS_CODE.forEach((code) => {
-    it(`Should install and work with the Typescript code:\n${code}`, async function() {
+  TS_CODE_ARRAY.forEach((code) => {
+    it(`should install and work with the Typescript code:\n${code}`, async function() {
       this.timeout(TIMEOUT_MS);
       assert(installDir);
       await writeFileP(path.join(installDir!, INDEX_TS), code, 'utf-8');
@@ -83,8 +98,8 @@ describe('Installation', () => {
     });
   });
 
-  JS_CODE.forEach((code) => {
-    it(`Should install and work with the Javascript code:\n${code}`, async function() {
+  JS_CODE_ARRAY.forEach((code) => {
+    it(`should install and work with the Javascript code:\n${code}`, async function() {
       this.timeout(TIMEOUT_MS);
       assert(installDir);
       await writeFileP(path.join(installDir!, INDEX_JS), code, 'utf-8');
