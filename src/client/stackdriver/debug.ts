ce903bcf7d7638dcbc33927a94c758581166e906
F. Hinkelmann <fhinkel@vt.edu>
2018-08-30 12:52:59 -0400

    feat: use small HTTP dependency (#510)
    Improve load time from 1000 ms to under 800 ms.
    
    Drive-by fix: Delete unused import.

--- src/client/stackdriver/debug.ts
+++ src/client/stackdriver/debug.ts
@@ -15,7 +15,8 @@
  */
 
 import {GoogleAuthOptions, Service} from '@google-cloud/common';
-import * as request from 'request';
+import * as request from 'request';  //  Only for type declarations.
+import {teenyRequest} from 'teeny-request';
 
 export interface PackageInfo {
   name: string;
@@ -57,7 +58,7 @@ export class Debug extends Service {
     }
 
     const config = {
-      requestModule: request,
+      requestModule: teenyRequest as typeof request,
       projectIdRequired: false,
       baseUrl: 'https://clouddebugger.googleapis.com/v2',
       scopes: ['https://www.googleapis.com/auth/cloud_debugger'],
