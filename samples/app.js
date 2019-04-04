50abf1db3ed6f8f24b26e8b9829e59e33560d99b
Justin Beckwith <justin.beckwith@gmail.com>
2019-04-04 08:39:44 -0700

    test: add smoke test for app sample (#663)

--- samples/app.js
+++ samples/app.js
@@ -28,12 +28,8 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-if (module === require.main) {
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
-  });
-}
-
-module.exports = app;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
