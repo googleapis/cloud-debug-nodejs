96a22105b8e15076cc89467dde800e611d483116
Dominic Kramer <dominicdkramer@google.com>
2019-05-28 10:29:24 -0700

    feat: add `get()` function
    The `get()` function is used to access an agent if it
    has been started.

--- test/test-module.ts
+++ test/test-module.ts
@@ -15,9 +15,10 @@
  */
 
 import * as assert from 'assert';
-const m: NodeModule & {start: Function} = require('../..');
+const m: NodeModule & {start: Function, get: () => Debuglet|undefined} = require('../..');
 import * as nock from 'nock';
 import * as nocks from './nocks';
+import { Debuglet } from '../src/agent/debuglet';
 
 nock.disableNetConnect();
 
@@ -38,5 +39,18 @@ describe('Debug module', () => {
     assert.throws(() => {
       m.start();
     });
+  });
+
+  it('should return the agent via the get() method', () => {
+    const agent = m.get();
+    assert(agent, 'Expected to get the started agent');
+    assert.strictEqual(agent!.config.projectId, '0');
+  });
+});
+
+describe('Debug module without start() called', () => {
+  it('get() should return `undefined`', () => {
+    const agent = m.get();
+    assert.strictEqual(agent, undefined, 'Expected `undefined` since the agent was not started');
   });
 });
