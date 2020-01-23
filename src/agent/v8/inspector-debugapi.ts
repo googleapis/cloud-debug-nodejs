2e0ac8999a7fa08f0ead2d3233dc4f6709221265
Dominic Kramer <DominicDKramer@google.com>
2020-01-22 20:42:20 -0800

    fix: breakpoints work on windows
    On Windows the URL provided to the inspector API must start with
    file:/// instead of file:// and all backslashes must be converted
    to forward slashes.

--- src/agent/v8/inspector-debugapi.ts
+++ src/agent/v8/inspector-debugapi.ts
@@ -489,9 +489,14 @@ export class InspectorDebugApi implements debugapi.DebugApi {
     let v8BreakpointId; // v8/inspector breakpoint id
     if (!this.locationMapper[locationStr]) {
       // The first time when a breakpoint was set to this location.
-      const url = this.inspectorOptions.useWellFormattedUrl
+      const rawUrl = this.inspectorOptions.useWellFormattedUrl
         ? `file://${matchingScript}`
         : matchingScript;
+      // on windows the url must start with file:/// (notice 3 slashes)
+      // and have all backslashes converted into forward slashes
+      const url = process.platform === 'win32'
+        ? rawUrl.replace(/^file:\/\//, 'file:///').replace(/\\/g, '/')
+        : rawUrl;
       const res = this.v8Inspector.setBreakpointByUrl({
         lineNumber: line - 1,
         url,
