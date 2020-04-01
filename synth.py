68629206c974d67c74c9474ad43a69eb47abedc8
Benjamin Coe <bencoe@google.com>
2020-03-31 18:00:08 -0700

    build: set AUTOSYNTH_MULTIPLE_COMMITS=true for context aware commits

--- synth.py
+++ synth.py
@@ -18,6 +18,9 @@ import logging
 import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
+
+AUTOSYNTH_MULTIPLE_COMMITS = True
+
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
 s.copy(templates)
